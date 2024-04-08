c1e10745917eb505aef08a42f9f1d88c3b76b860
Jean Flaherty <kobejean@me.com>
2024-04-09 01:35:07 +0900

    m

--- nerf_qa/DISTS_pytorch/DISTS_pt.py
+++ nerf_qa/DISTS_pytorch/DISTS_pt.py
@@ -80,7 +80,7 @@ class DISTS(torch.nn.Module):
             self.beta.data = weights['beta']
 
     def project_weights(self):
-        lower_bound = torch.zeros_like(self.alpha.data) + 1e-8
+        lower_bound = torch.zeros_like(self.alpha.data)
         lower_bound[:,:3,:,:] = 0.02
         alpha = torch.max(self.alpha.data, lower_bound)
         beta = torch.max(self.beta.data, lower_bound)
