f786b3c8797064fd8590830b350588e7b3dc0c3e
Jean Flaherty <kobejean@me.com>
2024-04-09 22:24:37 +0900

    m

--- run_nerf_qa.py
+++ run_nerf_qa.py
@@ -124,175 +124,175 @@ if __name__ == '__main__':
     groups = scores_df['scene']
     step = 0
 
-    cv_correlations = []
-    cv_scene_mins = []
-    cv_last_mses = []
-    cv_last_losses = []
+    # cv_correlations = []
+    # cv_scene_mins = []
+    # cv_last_mses = []
+    # cv_last_losses = []
 
-    # Create splits
-    for fold, (train_idx, val_idx) in enumerate(gkf.split(scores_df, groups=groups)):
-        train_df = scores_df.iloc[train_idx].reset_index(drop=True)
-        val_df = scores_df.iloc[val_idx].reset_index(drop=True)
+    # # Create splits
+    # for fold, (train_idx, val_idx) in enumerate(gkf.split(scores_df, groups=groups)):
+    #     train_df = scores_df.iloc[train_idx].reset_index(drop=True)
+    #     val_df = scores_df.iloc[val_idx].reset_index(drop=True)
 
-        train_logger = MetricCollectionLogger(f'Train Metrics Dict/fold_{fold}')
-        val_logger = MetricCollectionLogger(f'Val Metrics Dict/fold_{fold}')
+    #     train_logger = MetricCollectionLogger(f'Train Metrics Dict/fold_{fold}')
+    #     val_logger = MetricCollectionLogger(f'Val Metrics Dict/fold_{fold}')
 
-        train_dataloader = create_nerf_qa_resize_dataloader(train_df, dir=DATA_DIR, batch_size=DEVICE_BATCH_SIZE)
-        val_dataloader = create_nerf_qa_resize_dataloader(val_df, dir=DATA_DIR, batch_size=DEVICE_BATCH_SIZE, scene_balanced=False)
-        train_size = len(train_dataloader)
-        val_size = len(val_dataloader)
+    #     train_dataloader = create_nerf_qa_resize_dataloader(train_df, dir=DATA_DIR, batch_size=DEVICE_BATCH_SIZE)
+    #     val_dataloader = create_nerf_qa_resize_dataloader(val_df, dir=DATA_DIR, batch_size=DEVICE_BATCH_SIZE, scene_balanced=False)
+    #     train_size = len(train_dataloader)
+    #     val_size = len(val_dataloader)
 
-        batch_step = 0
+    #     batch_step = 0
 
-        # Reset model and optimizer for each fold (if you want to start fresh for each fold)
-        model = NeRFQAModel(train_df=train_df, mode=config.mode).to(device)
-        if config.optimizer == 'sadamw':
-            optimizer = schedulefree.AdamWScheduleFree(model.parameters(),                
-                lr=config.lr,
-                betas=(config.beta1, config.beta2),
-                eps=config.eps,
-                warmup_steps=config.warmup_steps,
-            )
-        else:
-            optimizer = optim.Adam(model.parameters(),
-                lr=config.lr,
-                betas=(config.beta1, config.beta2),
-                eps=config.eps,
-            )
-            scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.epochs - config.warmup_steps/train_size, eta_min=0, last_epoch=-1)
+    #     # Reset model and optimizer for each fold (if you want to start fresh for each fold)
+    #     model = NeRFQAModel(train_df=train_df, mode=config.mode).to(device)
+    #     if config.optimizer == 'sadamw':
+    #         optimizer = schedulefree.AdamWScheduleFree(model.parameters(),                
+    #             lr=config.lr,
+    #             betas=(config.beta1, config.beta2),
+    #             eps=config.eps,
+    #             warmup_steps=config.warmup_steps,
+    #         )
+    #     else:
+    #         optimizer = optim.Adam(model.parameters(),
+    #             lr=config.lr,
+    #             betas=(config.beta1, config.beta2),
+    #             eps=config.eps,
+    #         )
+    #         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.epochs - config.warmup_steps/train_size, eta_min=0, last_epoch=-1)
 
 
-        # Training loop
-        for epoch in range(wandb.config.epochs):
-            print(f"Epoch {epoch+1}/{wandb.config.epochs}")
+    #     # Training loop
+    #     for epoch in range(wandb.config.epochs):
+    #         print(f"Epoch {epoch+1}/{wandb.config.epochs}")
 
-            # Train step
-            model.train()  # Set model to training mode
-            if config.optimizer == 'sadamw':
-                optimizer.train()
+    #         # Train step
+    #         model.train()  # Set model to training mode
+    #         if config.optimizer == 'sadamw':
+    #             optimizer.train()
 
-            for dist,ref,score,i in tqdm(train_dataloader, total=train_size, desc="Training..."):  # Start index from 1 for easier modulus operation 
-                if config.optimizer != 'sadamw':
-                    if batch_step < config.warmup_steps:
-                        warmup_lr = config.lr * 1e-4 + batch_step * (config.lr - config.lr * 1e-4) / config.warmup_steps
-                        for param_group in optimizer.param_groups:
-                            param_group['lr'] = warmup_lr  
-                    elif batch_step == config.warmup_steps:
-                        scheduler.last_epoch = epoch        
+    #         for dist,ref,score,i in tqdm(train_dataloader, total=train_size, desc="Training..."):  # Start index from 1 for easier modulus operation 
+    #             if config.optimizer != 'sadamw':
+    #                 if batch_step < config.warmup_steps:
+    #                     warmup_lr = config.lr * 1e-4 + batch_step * (config.lr - config.lr * 1e-4) / config.warmup_steps
+    #                     for param_group in optimizer.param_groups:
+    #                         param_group['lr'] = warmup_lr  
+    #                 elif batch_step == config.warmup_steps:
+    #                     scheduler.last_epoch = epoch        
                     
-                    for it, param_group in enumerate(optimizer.param_groups):
-                        wandb.log({ f'Optimizer/lr_{it}': param_group['lr'] }, step = step)
-                optimizer.zero_grad()  # Zero the gradients after updating
+    #                 for it, param_group in enumerate(optimizer.param_groups):
+    #                     wandb.log({ f'Optimizer/lr_{it}': param_group['lr'] }, step = step)
+    #             optimizer.zero_grad()  # Zero the gradients after updating
 
-                # Load scores
-                predicted_score = model(dist.to(device),ref.to(device))
-                target_score = score.to(device).float()
+    #             # Load scores
+    #             predicted_score = model(dist.to(device),ref.to(device))
+    #             target_score = score.to(device).float()
                 
-                # Compute loss
-                loss = loss_fn(predicted_score, target_score)
-                step += score.shape[0]
-                batch_step += 1
+    #             # Compute loss
+    #             loss = loss_fn(predicted_score, target_score)
+    #             step += score.shape[0]
+    #             batch_step += 1
 
-                # Store metrics in logger
-                scene_ids =  train_df['scene'].iloc[i.numpy()].values
-                video_ids =  train_df['distorted_filename'].iloc[i.numpy()].values
-                train_logger.add_entries(
-                    {
-                    'loss': loss.detach().cpu(),
-                    'mse': mse_fn(predicted_score, target_score).detach().cpu(),
-                }, video_ids = video_ids, scene_ids = scene_ids)
+    #             # Store metrics in logger
+    #             scene_ids =  train_df['scene'].iloc[i.numpy()].values
+    #             video_ids =  train_df['distorted_filename'].iloc[i.numpy()].values
+    #             train_logger.add_entries(
+    #                 {
+    #                 'loss': loss.detach().cpu(),
+    #                 'mse': mse_fn(predicted_score, target_score).detach().cpu(),
+    #             }, video_ids = video_ids, scene_ids = scene_ids)
 
-                # Accumulate gradients
-                loss = loss.mean()
-                loss.backward()
+    #             # Accumulate gradients
+    #             loss = loss.mean()
+    #             loss.backward()
 
-                # Log accumulated train metrics
-                train_logger.log_summary(step)
+    #             # Log accumulated train metrics
+    #             train_logger.log_summary(step)
                 
-                # Update parameters every batches_per_step steps or on the last iteration
-                optimizer.step()
-                if config.project_weights == 'True':
-                    model.dists_model.project_weights()
+    #             # Update parameters every batches_per_step steps or on the last iteration
+    #             optimizer.step()
+    #             if config.project_weights == 'True':
+    #                 model.dists_model.project_weights()
                     
-            if config.optimizer != 'sadamw':
-                scheduler.step()
+    #         if config.optimizer != 'sadamw':
+    #             scheduler.step()
 
-            # Validation step
-            model.eval()  # Set model to evaluation mode
+    #         # Validation step
+    #         model.eval()  # Set model to evaluation mode
 
-            if config.optimizer == 'sadamw':
-                optimizer.eval()
-            with torch.no_grad():
-                for dist, ref, score, i in tqdm(val_dataloader, total=val_size, desc="Validating..."):
-                    # Compute score
-                    predicted_score = model(dist.to(device), ref.to(device))
-                    target_score = score.to(device).float()
+    #         if config.optimizer == 'sadamw':
+    #             optimizer.eval()
+    #         with torch.no_grad():
+    #             for dist, ref, score, i in tqdm(val_dataloader, total=val_size, desc="Validating..."):
+    #                 # Compute score
+    #                 predicted_score = model(dist.to(device), ref.to(device))
+    #                 target_score = score.to(device).float()
 
-                    # Compute loss
-                    loss = loss_fn(predicted_score, target_score)
+    #                 # Compute loss
+    #                 loss = loss_fn(predicted_score, target_score)
                     
-                    # Store metrics in logger
-                    scene_ids = val_df['scene'].iloc[i.numpy()].values
-                    video_ids = val_df['distorted_filename'].iloc[i.numpy()].values
-                    val_logger.add_entries(
-                        {
-                        'loss': loss.detach().cpu(),
-                        'mse': mse_fn(predicted_score, target_score).detach().cpu(),
-                        'mos': score,
-                        'pred_score': predicted_score.detach().cpu(),
-                    }, video_ids = video_ids, scene_ids = scene_ids)
+    #                 # Store metrics in logger
+    #                 scene_ids = val_df['scene'].iloc[i.numpy()].values
+    #                 video_ids = val_df['distorted_filename'].iloc[i.numpy()].values
+    #                 val_logger.add_entries(
+    #                     {
+    #                     'loss': loss.detach().cpu(),
+    #                     'mse': mse_fn(predicted_score, target_score).detach().cpu(),
+    #                     'mos': score,
+    #                     'pred_score': predicted_score.detach().cpu(),
+    #                 }, video_ids = video_ids, scene_ids = scene_ids)
 
-                # Log accumulated metrics
-                val_logger.log_summary(step)
-                wandb.log({ 
-                    "Model/dists_weight/alpha": wandb.Histogram(model.dists_model.alpha.detach().cpu()),
-                    "Model/dists_weight/beta": wandb.Histogram(model.dists_model.beta.detach().cpu()),
-                    "Model/dists_weight/alpha_min": torch.min(model.dists_model.alpha),
-                    "Model/dists_weight/beta_min": torch.min(model.dists_model.beta),
-                }, step=step)
+    #             # Log accumulated metrics
+    #             val_logger.log_summary(step)
+    #             wandb.log({ 
+    #                 "Model/dists_weight/alpha": wandb.Histogram(model.dists_model.alpha.detach().cpu()),
+    #                 "Model/dists_weight/beta": wandb.Histogram(model.dists_model.beta.detach().cpu()),
+    #                 "Model/dists_weight/alpha_min": torch.min(model.dists_model.alpha),
+    #                 "Model/dists_weight/beta_min": torch.min(model.dists_model.beta),
+    #             }, step=step)
         
-        cv_correlations.append(val_logger.last_correlations)
-        cv_scene_mins.append(val_logger.last_scene_min)
-        cv_last_mses.append(val_logger.last_mse)
-        cv_last_losses.append(val_logger.last_loss)
+    #     cv_correlations.append(val_logger.last_correlations)
+    #     cv_scene_mins.append(val_logger.last_scene_min)
+    #     cv_last_mses.append(val_logger.last_mse)
+    #     cv_last_losses.append(val_logger.last_loss)
 
-    cv_correlations_concat = {}
-    cv_scene_mins_concat = {}
+    # cv_correlations_concat = {}
+    # cv_scene_mins_concat = {}
 
-    # Loop through each dictionary in the list
-    for scores in cv_correlations:
-        for key, value in scores.items():
-            if key in cv_correlations_concat:
-                cv_correlations_concat[key].append(value)
-            else:
-                cv_correlations_concat[key] = [value]
+    # # Loop through each dictionary in the list
+    # for scores in cv_correlations:
+    #     for key, value in scores.items():
+    #         if key in cv_correlations_concat:
+    #             cv_correlations_concat[key].append(value)
+    #         else:
+    #             cv_correlations_concat[key] = [value]
     
-    # Loop through each dictionary in the list
-    for scores in cv_scene_mins:
-        for key, value in scores.items():
-            if key in cv_scene_mins_concat:
-                cv_scene_mins_concat[key].append(value)
-            else:
-                cv_scene_mins_concat[key] = [value]
+    # # Loop through each dictionary in the list
+    # for scores in cv_scene_mins:
+    #     for key, value in scores.items():
+    #         if key in cv_scene_mins_concat:
+    #             cv_scene_mins_concat[key].append(value)
+    #         else:
+    #             cv_scene_mins_concat[key] = [value]
 
 
-    for key, value in cv_correlations_concat.items():
-        wandb.log({ 
-            f"Cross-Val Metrics Dict/correlations/mean_{key}": np.mean(value),
-            f"Cross-Val Metrics Dict/correlations/std_{key}": np.std(value),
-        }, step=step)
+    # for key, value in cv_correlations_concat.items():
+    #     wandb.log({ 
+    #         f"Cross-Val Metrics Dict/correlations/mean_{key}": np.mean(value),
+    #         f"Cross-Val Metrics Dict/correlations/std_{key}": np.std(value),
+    #     }, step=step)
 
-    for key, value in cv_scene_mins_concat.items():
-        wandb.log({ 
-            f"Cross-Val Metrics Dict/correlations/scene_min/mean_{key}": np.mean(value),
-            f"Cross-Val Metrics Dict/correlations/scene_min/std_{key}": np.std(value),
-        }, step=step)
-    wandb.log({ 
-        f"Cross-Val Metrics Dict/mean_mse": np.mean(cv_last_mses),
-        f"Cross-Val Metrics Dict/std_mse": np.std(cv_last_mses),
-        f"Cross-Val Metrics Dict/mean_loss": np.mean(cv_last_losses),
-        f"Cross-Val Metrics Dict/std_loss": np.std(cv_last_losses),
-    }, step=step)
+    # for key, value in cv_scene_mins_concat.items():
+    #     wandb.log({ 
+    #         f"Cross-Val Metrics Dict/correlations/scene_min/mean_{key}": np.mean(value),
+    #         f"Cross-Val Metrics Dict/correlations/scene_min/std_{key}": np.std(value),
+    #     }, step=step)
+    # wandb.log({ 
+    #     f"Cross-Val Metrics Dict/mean_mse": np.mean(cv_last_mses),
+    #     f"Cross-Val Metrics Dict/std_mse": np.std(cv_last_mses),
+    #     f"Cross-Val Metrics Dict/mean_loss": np.mean(cv_last_losses),
+    #     f"Cross-Val Metrics Dict/std_loss": np.std(cv_last_losses),
+    # }, step=step)
 
 
     train_df = scores_df
