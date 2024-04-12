c9e27dcc651865ef7ed4c016105998e247617008
Jean Flaherty <kobejean@me.com>
2024-04-12 23:04:33 +0900

    m

--- run_final.py
+++ run_final.py
@@ -243,17 +243,17 @@ if __name__ == '__main__':
         # Train step
         model.train()
         
-        wandb.log({ f'Optimizer/lr': optimizer.param_groups[0]['lr'] }, step = step)
 
         for dist,ref,score,i in tqdm(train_dataloader, total=train_size, desc="Training..."): 
-            if epoch == 0:
-                warmup_lr_init_lr = config.lr * 1e-4
-                warmup_lr = warmup_lr_init_lr + step * (config.lr - warmup_lr_init_lr) / train_size
-                for param_group in optimizer.param_groups:
-                    param_group['lr'] = warmup_lr  
-            elif epoch == 1:
-                scheduler.last_epoch = epoch          
-            optimizer.zero_grad()  # Zero the gradients after updating
+            # if epoch == 0:
+            #     warmup_lr_init_lr = config.lr * 1e-4
+            #     warmup_lr = warmup_lr_init_lr + step * (config.lr - warmup_lr_init_lr) / train_size
+            #     for param_group in optimizer.param_groups:
+            #         param_group['lr'] = warmup_lr  
+            # elif epoch == 1:
+            #     scheduler.last_epoch = epoch          
+            wandb.log({ f'Optimizer/lr': optimizer.param_groups[0]['lr'] }, step = step)
+            optimizer.zero_grad(set_to_none=True)  # Zero the gradients after updating
 
             predicted_score, dists_score = model(dist.to(device),ref.to(device))
             target_score = score.to(device).float()
