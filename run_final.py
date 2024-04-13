80760e55716f5b529e05c642e4f1ff1d0140d4df
Jean Flaherty <kobejean@me.com>
2024-04-13 14:42:11 +0900

    m

--- run_final.py
+++ run_final.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 
     # Basic configurations
     parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-    parser.add_argument('--lr', type=float, default=1e-6, help='Random seed.')
+    parser.add_argument('--lr', type=float, default=5e-5, help='Random seed.')
     parser.add_argument('--beta1', type=float, default=0.9, help='Random seed.')
     parser.add_argument('--beta2', type=float, default=0.999, help='Random seed.')
     parser.add_argument('--eps', type=float, default=1e-7, help='Random seed.')
